54109a1368d8d52827446da29283a55a46de519c
Lightsider <andrew9727@mail.ru>
2017-07-13 14:53:35 +0700

    13.07.2017 15:00 Добавлена работа через Grid

--- src/test/java/basesrc/core/Testbase.java
+++ src/test/java/basesrc/core/Testbase.java
@@ -1,17 +1,24 @@
 package basesrc.core;
 
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
+
+import java.net.URL;
+
 
 public class Testbase {
     private AppManager app;
 
     @BeforeClass(alwaysRun = true)
-    public void init(){
+    public void init() throws  Exception {
+        //DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        //capabilities.setBrowserName("firefox");
+        //capabilities.setPlatform(Platform.LINUX);
         app = new AppManager();
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\andrey.popov\\IdeaProjects\\mytest\\geckodriver.exe");
-        app.setDriver(new FirefoxDriver());
+        //System.setProperty("webdriver.gecko.driver", "C:\\Users\\andrey.popov\\IdeaProjects\\mytest\\geckodriver.exe");
+        app.setDriver(new RemoteWebDriver(new URL("http://192.168.99.100:32770/wd/hub"),DesiredCapabilities.firefox()));
         app.setWait(new WebDriverWait(app.getDriver(),12));
     }
 
